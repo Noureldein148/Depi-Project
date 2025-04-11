ebe1da9b2212af0bab268e4e24173f81feadbcfc
GitGenius24 <mm4754809@gmail.com>
2025-04-11 19:10:37 +0200

    first commit

---
+++ MVC DEPI Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MVC_DEPI_Project.Models;
+
+namespace MVC_DEPI_Project.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<ApplicationUser>().ToTable("Users", "Security");
+            builder.Entity<IdentityRole>().ToTable("Roles", "Security");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles", "Security");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims", "Security");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogin", "Security");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("UserRolesClaim", "Security");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserToken", "Security");
+
+        }
+    }
+}
